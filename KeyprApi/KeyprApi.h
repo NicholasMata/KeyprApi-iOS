81a2bc4636c34d674625d4d4b815a7edb2233e4d
Nicholas Mata <nicholasmata94@gmail.com>
2018-11-27 20:04:32 -0800

    Initial Commit

---
+++ KeyprApi/KeyprApi.h
@@ -0,0 +1,19 @@
+//
+//  KeyprApi.h
+//  KeyprApi
+//
+//  Created by Nicholas Mata on 11/27/18.
+//  Copyright © 2018 Nicholas Mata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyprApi.
+FOUNDATION_EXPORT double KeyprApiVersionNumber;
+
+//! Project version string for KeyprApi.
+FOUNDATION_EXPORT const unsigned char KeyprApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyprApi/PublicHeader.h>
+
+
